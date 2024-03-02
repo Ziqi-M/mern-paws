a12adc53100ef4a647223df51855b11abc2c33c1
xiaobaocute <weicureco@gmail.com>
2024-03-01 23:37:38 -0800

    complete create post functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,19 +1,18 @@
-import { errorHandler } from "../utils/error.js";
-import Post from "../models/post.model.js";
-import User from "../models/user.model.js";
+import Post from '../models/post.model.js';
+import { errorHandler } from '../utils/error.js';
 
 export const create = async (req, res, next) => {
   if (!req.user.isAdmin) {
-    return next(errorHandler(403, "You are not allowed to create a post"));
+    return next(errorHandler(403, 'You are not allowed to create a post'));
   }
   if (!req.body.title || !req.body.content) {
-    return next(errorHandler(400, "Title and content are required"));
+    return next(errorHandler(400, 'Please provide all required fields'));
   }
   const slug = req.body.title
-    .split(" ")
-    .join("-")
+    .split(' ')
+    .join('-')
     .toLowerCase()
-    .replace(/[^a-zA-Z0-9-]/g, "");
+    .replace(/[^a-zA-Z0-9-]/g, '');
   const newPost = new Post({
     ...req.body,
     slug,
@@ -21,8 +20,8 @@ export const create = async (req, res, next) => {
   });
   try {
     const savedPost = await newPost.save();
-    res.status(201).json({ savedPost });
+    res.status(201).json(savedPost);
   } catch (error) {
-    next(errorHandler(500, error.message));
+    next(error);
   }
 };
